3a1404b8f6d8082922c6af5cc9da8c745024b9c3
jaiden <jaident22@nycstudents.net>
2026-03-12 12:38:34 -0400

    done

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Success! You have been signed up for the Anime Club!');
+});
 
